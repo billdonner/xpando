84955d4cc614e603e4badad327298ba47ea24a17
bill donner <billdonner@gmail.com>
2024-01-03 18:26:46 -0600

    was bender now 0.1.5

---
+++ README.md
@@ -0,0 +1,25 @@
+#  XPANDO - make ReadyForIOS1
+version 0.1.5
+
+## Takes Multiple Outputs from T9 and Bends Them Into IOS APP Format 
+
+## Command Line
+
+
+```
+OVERVIEW: Bender Builds The Files Needed By QANDA Mobile App
+
+USAGE: bender <json-files> ... [--output-file <output-file>] [--td-path <td-path>]
+
+ARGUMENTS:
+  <json-files>            The data files to be merged.
+
+OPTIONS:
+  -o, --output-file <output-file>
+                          The name of the output file. (default:
+                          merged_output.json)
+  -t, --td-path <td-path> The name of the topics data file . (default:
+                          TopicData.json)
+  --version               Show the version.
+  -h, --help              Show help information.
+```
--- xpando/main.swift
+++ xpando/main.swift
@@ -6,6 +6,232 @@
 //
 
 import Foundation
+import ArgumentParser
+import q20kshare
 
-print("Hello, World!")
+extension Challenge:Comparable {
+  public static func < (lhs:  Challenge, rhs:  Challenge) -> Bool {
+    lhs.question < rhs.question
+  }
+}
+func blend(_ mergedData:[Challenge],tdPath:String) throws -> PlayData {
+  
+  func fetchTopicData(_ tdurl:String ) throws -> TopicData {
+    // Load substitutions JSON file,throw out all of the metadata for now
+    let xdata = try Data(contentsOf: URL(fileURLWithPath: tdurl))
+    let decoded = try JSONDecoder().decode(TopicData.self, from:xdata)
+    return decoded
+  }
 
+  
+  let topicData = try fetchTopicData(tdPath)
+  let tdblocks = topicData.topics
+  var dedupedData: [Challenge] = []
+  // dedupe phase I = sort by ID then by reverse time
+  let x = mergedData.sorted(by:) {
+    if $0.id < $1.id { return true }
+    else if $0.id > $1.id { return false }
+    else { // equal id
+      return $0.date > $1.date
+    }
+  }
+  var lastid = ""
+  // dont copy if same as last id
+  for d in x {
+    if d.id != lastid {
+      dedupedData.append(d)
+      lastid = d.id
+    }
+  }
+  // now sort by topic and time
+  dedupedData.sort(by:) {
+    if $0.topic < $1.topic { return true }
+    else if $0.topic > $1.topic { return false }
+    else { // equal id
+      return $0.date < $1.date
+    }
+  }
+  if dedupedData.count != mergedData.count {
+    print("\(mergedData.count - dedupedData.count) duplicates removed")
+  }
+  // now produce a topic manifest
+  struct Entry {
+    var topic:String
+    var count:Int
+  }
+  var lasttopic = ""
+  var topicitems = 0
+  var entries:[Entry] = []
+  for d in dedupedData {
+    if d.topic != lasttopic {
+      if topicitems != 0 {
+        entries.append(Entry(topic: lasttopic,count: topicitems))
+      }
+      lasttopic = d.topic
+      topicitems = 1
+    } else {
+      topicitems += 1
+    }
+  }
+  if topicitems != 0 {
+    entries.append(Entry(topic: lasttopic,count: topicitems))
+  }
+  print("+======TOPICS======+")
+  for e in entries {
+    print (" \(e.topic)   \(e.count)  ")
+  }
+  print("+==================+")
+  
+  let topics =  entries.map {
+    var pic = "pencil"
+    var notes = "Notes for \(pic)"
+    for td in tdblocks {
+      if $0.topic == td.name { pic = td.pic ; notes = td.notes; break}
+    }
+    return Topic(name: $0.topic, subject: $0.topic, pic:pic,   notes: notes)
+  }
+ 
+  let rewrittenTd = TopicData(description:topicData.description,version:topicData.version,
+                     author:topicData.author, date: "\(Date())",
+                     purpose:topicData.purpose,topics:topics)
+  
+  var gamedatum: [GameData] = []
+  for t in topics {
+    var challenges:[Challenge] = []
+    // crude
+    for item in dedupedData {
+      if item.topic == t.name  {
+        challenges.append(item)//.makeChallenge())
+      }
+    }
+    let gda = GameData(topic: t.name, challenges: challenges)
+    gamedatum.append(gda)
+  }
+
+  let playdata = PlayData(topicData:rewrittenTd,
+                          gameDatum: gamedatum,
+                          playDataId: UUID().uuidString,
+                          blendDate: Date() )
+  
+return playdata
+}
+func expand(dirPaths: [String], filterCallback: (String,String ) -> Bool) {
+    let fileManager = FileManager.default
+    
+    for dirPath in dirPaths {
+        guard let dirContents = try? fileManager.contentsOfDirectory(atPath: dirPath) else {
+            continue
+        }
+        let fullPaths = dirContents.map { "\($0)"} //{ dirPath.appending("/\($0)") }
+        let _ = fullPaths.filter { filterCallback(dirPath.appending("/\($0)") , $0) }
+
+    }
+}
+func contained(_ string: String, within: String) -> Bool {
+    return within.range(of: string, options: .caseInsensitive) != nil
+}
+ 
+struct Xpando: ParsableCommand {
+  
+  static let configuration = CommandConfiguration(
+    abstract: "XPANDO Builds The Files Needed By QANDA Mobile App and More",
+    version: "0.1.5",
+    subcommands: [],
+    defaultSubcommand: nil,
+    helpNames: [.long, .short]
+  )
+
+  @Argument(help: "List of directory paths.")
+  var directoryPaths: [String]
+  @Option(help:"filter string")
+  var filter = ""
+  @Option(help:"quiet")
+  var quiet = false
+  @Option(help:"dedupe")
+  var dedupe = true
+  @Option(name: .shortAndLong, help: "The name of the topics data file .")
+  var tdPath: String = "TopicData.json"
+  @Option(name: .shortAndLong, help: "The name of the ios output file.")
+  var outputFile: String = "readyforiosx.json"
+  
+    var processed = 0
+    var included = 0
+    var allQuestions:[Challenge] = []
+  
+    mutating func run() throws {
+      let decoder = JSONDecoder()
+      let allfilters = filter == "" ? []:filter.components(separatedBy: ",")
+      print(">Processing: ",directoryPaths.joined(separator:","))
+      print(">Filters: ",allfilters.joined(separator: ","))
+        expand(dirPaths: directoryPaths) { fullpath ,filename in
+          processed += 1
+            // Your filter condition goes here
+          // apply the filename filter
+          var include = false
+          if allfilters.count == 0 {
+            include = true
+          } else {
+            for thefilter in allfilters {
+              if contained(thefilter,within: filename) {
+                include = true ; break
+              }
+            }
+          }
+           if include  {
+            included += 1
+             if dedupe {
+               // open the file to get to the actual challenge
+               if let data = try? Data(contentsOf: URL(fileURLWithPath: fullpath)) {
+                 if let challenge = try? decoder.decode(Challenge.self,from:data) {
+                   if !quiet {
+                     print (challenge.question, ",",challenge.id)
+                   }
+                   allQuestions.append(challenge)
+                 }
+               }
+             } else {
+               // not deduping
+               if !quiet {
+                 print (">selected: " + fullpath)
+               }
+             }
+          }
+          return include
+        }
+      print(">Filter string: \(filter) selected \(included) of \(processed)")
+      if dedupe {
+        var dupes = 0
+        allQuestions.sort()
+        var last : Challenge? = nil
+        for q in allQuestions  {
+          if let last = last  {
+            if last == q {
+              print (last.question,"with id:",last.id," has duplicate with id:",q.id)
+              dupes += 1
+            }
+          }
+          last = q
+        }
+        print(">Exact Duplicates detected: \(dupes)")
+        // now blend for ios
+        if outputFile != "" {
+          let playdata = try blend(allQuestions, tdPath: tdPath)
+          // write the deduped data
+          let encoder = JSONEncoder()
+          // encoder.dateEncodingStrategy = .iso8601
+          encoder.outputFormatting = .prettyPrinted
+          do {
+            let outputData = try encoder.encode(playdata)
+            let outurl = URL(fileURLWithPath: outputFile)
+            try? outputData.write(to: outurl)
+            print("Data files merged successfully - \(allQuestions.count) saved to \(outputFile)")
+          }
+          catch {
+            print("Encoding error: \(error)")
+          }
+        }
+      }
+    }
+}
+
+Xpando.main()
